8f6a1d0c03617fc38b390e0ca5fea80583b9a8b3
madina <141180038+Vitalian2@users.noreply.github.com>
2023-09-03 12:13:30 +0600

    Update postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -1,21 +1,21 @@
 asdfasdfasdfasdfasdfasdfasasdfasdfsadasdfasdfsadfsdf312312312
 cv
   zcvasdfasdfasdf
-    zc
-      vz
+    zcD
+      vzFA
         z
   cv
-    zcv
+    zcvfDSA
       xbxcvbxcv12312312adfasdfasdfasdfasdfasdfsadfasdfasdfconst semver = require('semver');
 const colors = require('colors/safe');
 
 const { engines: { node: nodeVersion }} = require('./package');
 
 if (!semver.satisfies(process.version, nodeVersion)) {
-  process.emitWarning(
+  process.emitWarning(D
     colors.red(`
       For this task we are strictly recomend you to use node ${nodeVersion}.
       Now you are using node ${process.version}, if you are using any of features thaasdfasdfast not supported by node ${nodeVersion}, score won't be submitted
     `)
   );
-}
+}SAdasdas
